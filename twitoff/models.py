3f0657cad0532a827cbfcfaa2525a4ac5b2da70e
Alfaroque Islam <alfaroque5@gmail.com>
2020-07-21 18:55:00 -0400

    commit

--- twitoff/models.py
+++ twitoff/models.py
@@ -11,13 +11,26 @@ class User(DB.Model):
     """Twitter users."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
-        return 'User {}'.format(self.name)
+        return '[User {}]'.format(self.name)
+
+
+class Tweet(DB.Model):
+    """Tweets and their embeddings from Basilica."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))  # Allows for full tweet + link
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '[Tweet {}]'.format(self.text)
 
 
 def add_test_users():
     for i, name in enumerate(TWITTER_USERS):
         user = User(id=i, name=name)
         DB.session.add(user)
-    DB.session.commit() 
+    DB.session.commit()
